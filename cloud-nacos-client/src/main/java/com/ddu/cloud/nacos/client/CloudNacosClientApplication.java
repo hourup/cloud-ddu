b8038bc3b4ad6423af030848018dc1be359e451c
常浩然 <changhr@sense.com.cn>
2019-06-06 16:52:06 +0800

    [init]初始化工程

---
+++ cloud-nacos-client/src/main/java/com/ddu/cloud/nacos/client/CloudNacosClientApplication.java
@@ -0,0 +1,16 @@
+package com.ddu.cloud.nacos.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author changhr
+ */
+@SpringBootApplication
+public class CloudNacosClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNacosClientApplication.class, args);
+    }
+
+}
